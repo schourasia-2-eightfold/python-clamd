e5c2e03f347fa38a45c0bde44ffbda3f93736383
schourasia-2-eightfold <135719195+schourasia-2-eightfold@users.noreply.github.com>
2025-12-04 13:58:55 +0530

    replace send with sendall

--- src/clamd/__init__.py
+++ src/clamd/__init__.py
@@ -186,10 +186,10 @@ class ClamdNetworkSocket(object):
             chunk = buff.read(max_chunk_size)
             while chunk:
                 size = struct.pack(b'!L', len(chunk))
-                self.clamd_socket.send(size + chunk)
+                self.clamd_socket.sendall(size + chunk)
                 chunk = buff.read(max_chunk_size)
 
-            self.clamd_socket.send(struct.pack(b'!L', 0))
+            self.clamd_socket.sendall(struct.pack(b'!L', 0))
 
             result = self._recv_response()
 
